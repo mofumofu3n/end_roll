f1cc94570510ea01580d0f00152c3cee74ac79ab
mofumofu3n <mofumofu3n@gmail.com>
2016-03-25 05:22:02 +0900

    Require GLSlideShow

--- entry.js
+++ entry.js
@@ -1,6 +1,7 @@
 window.$ = window.jQuery = require('jquery');
 require('textillate');
 require('letteringjs')
+window.GLSlideshow = require('./js/GLSlideshow.js')
 
 $(function() {
   $('.tlt').textillate({
@@ -9,4 +10,15 @@ $(function() {
       sequence: true
     }
   });
+
+  var slideshow = GLSlideshow.autoDetectRenderer(
+    [ './img/1.jpg', './img/2.jpg'],
+    {
+      width: 1920,
+      height: 1080,
+      duration: 1000,     // optional
+      interval: 5000,     // optional
+      effect: 'wind' // optional
+    }
+  );  $('#background').append(slideshow.domElement);
 })
